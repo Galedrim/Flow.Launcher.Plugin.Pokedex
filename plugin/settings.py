575932ac291d6c4731b7c151b8b2dc8b0675ed3c
Galedrim <84284891+Galedrim@users.noreply.github.com>
2024-07-16 18:51:47 +0200

    :bug: Fix Settings File Path

--- plugin/settings.py
+++ plugin/settings.py
@@ -1,17 +1,23 @@
 import os
-import pathlib
 import json
 
 class Settings:
 
-    roamingDataPath = pathlib.Path(os.path.join(os.getenv("APPDATA"), "FlowLauncher/Settings/"))
-    settingsFilePath = os.path.join(roamingDataPath, "Settings.json")
+    plugins_dir = os.path.dirname(os.getcwd())
+    plugins_parent_dir = os.path.dirname(plugins_dir)
+    settings_file_path = os.path.join(plugins_parent_dir, "Settings", "Settings.json")
+
+    default_language = "en"
 
     def __init__(self):
         pass
 
     @staticmethod
     def get_language():
-        with open(Settings.settingsFilePath) as f:
-            data = json.load(f)
-        return data['Language']
+
+        if os.path.exists(Settings.settings_file_path):
+            with open(Settings.settings_file_path) as f:
+                data = json.load(f)
+                return data.get('Language', Settings.default_language)
+        else:
+            return Settings.default_language
