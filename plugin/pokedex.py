01e4ef1ddc845de1cd5206072135801daad004ab
Galedrim <84284891+Galedrim@users.noreply.github.com>
2024-07-18 16:32:02 +0200

    :art: Improve structure

--- plugin/pokedex.py
+++ plugin/pokedex.py
@@ -1,15 +1,14 @@
 import webbrowser
 
 from flox import Flox
-
 from settings import Settings
 from apiPokemon import ApiPokemon
 from sprites import Sprites
 
-COUP_CRITIQUE_ICON = ".\\Images\\coup_critique.png"
-SMOGON_ICON = ".\\Images\\smogon.png"
-POKEBIP_ICON = ".\\Images\\pokebip.png"
-BULBAPEDIA_ICON = ".\\Images\\bulbapedia.png"
+COUP_CRITIQUE_ICON = r".\Images\coup_critique.png"
+SMOGON_ICON = r".\Images\smogon.png"
+POKEBIP_ICON = r".\Images\pokebip.png"
+BULBAPEDIA_ICON = r".\Images\bulbapedia.png"
 
 class Pokedex(Flox):
 
@@ -17,27 +16,30 @@ class Pokedex(Flox):
         self.language = Settings.get_language()
         for pokemon in ApiPokemon.get_database():
             pokemon_name = ApiPokemon.get_names(pokemon)
-            if(self.check(pokemon_name['en'])):
-                if(self.language == "fr"):
-                        if any(self.match(query, value) for value in [pokemon_name['fr'], pokemon_name['en'], ApiPokemon.get_evolution(pokemon)]):
-                                self.add_item(
-                                    title=f"{pokemon_name['fr']} / {pokemon_name['en']} {ApiPokemon.get_types(pokemon)}",
-                                    subtitle=f'{ApiPokemon.get_evolution(pokemon)} {ApiPokemon.get_abilities(pokemon)}\n{ApiPokemon.get_stats(pokemon)}',
-                                    icon=Sprites.get_file(pokemon_name['en']),
-                                    context=pokemon_name,
-                                    method = self.open_url,
-                                    parameters=[f"https://www.pokebip.com/pokedex/pokemon/{pokemon_name['fr']}"]
-                                )
-                else:
-                    if self.match(query, pokemon_name['en']):
-                        self.add_item(
-                            title=f"{pokemon_name['en']} {ApiPokemon.get_types(pokemon)}",
-                            subtitle=f'{ApiPokemon.get_stats(pokemon)}',
-                            icon=Sprites.get_file(pokemon_name['en']),
-                            context=pokemon_name,
-                            method = self.open_url,
-                            parameters=[f"https://bulbapedia.bulbagarden.net/wiki/{pokemon_name['en']}_(Pokémon)"]
-                        )
+
+            if not self.match(query, pokemon_name['en']):
+                continue
+
+            if(self.language == "fr"):
+                    if any(self.match(query, value) for value in [pokemon_name['fr'], pokemon_name['en'], ApiPokemon.get_evolution(pokemon)]):
+                            self.add_item(
+                                title=f"{pokemon_name['fr']} / {pokemon_name['en']} {ApiPokemon.get_types(pokemon)}",
+                                subtitle=f'{ApiPokemon.get_evolution(pokemon)} {ApiPokemon.get_abilities(pokemon)}\n{ApiPokemon.get_stats(pokemon)}',
+                                icon=Sprites.get_file(pokemon_name['en']),
+                                context=pokemon_name,
+                                method = self.open_url,
+                                parameters=[f"https://www.pokebip.com/pokedex/pokemon/{pokemon_name['fr']}"]
+                            )
+            else:
+                if self.match(query, pokemon_name['en']):
+                    self.add_item(
+                        title=f"{pokemon_name['en']} {ApiPokemon.get_types(pokemon)}",
+                        subtitle=f'{ApiPokemon.get_stats(pokemon)}',
+                        icon=Sprites.get_file(pokemon_name['en']),
+                        context=pokemon_name,
+                        method = self.open_url,
+                        parameters=[f"https://bulbapedia.bulbagarden.net/wiki/{pokemon_name['en']}_(Pokémon)"]
+                    )
         return self._results
 
     def match(self, query, name):
@@ -47,11 +49,6 @@ class Pokedex(Flox):
         q = query.lower()
         if q in name.lower():
             return True
-
-    def check(self, name):
-        if name == 'MissingNo.':
-            return False
-        return True
 
     def context_menu(self, names):
         self.language = Settings.get_language()
