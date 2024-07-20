0dba7a7ceb8c285a7c8cbc415b2d82979301ecbc
Galedrim <84284891+Galedrim@users.noreply.github.com>
2024-07-20 03:35:02 +0200

    :lipstick: Update Pokemon Sprites

--- plugin/sprites.py
+++ plugin/sprites.py
@@ -1,10 +1,6 @@
 import os
-import re
-import json
 
-from urllib import request
-
-SPRITES_FOLDER = r".\sprites"
+SPRITES_DIR  = r".\sprites"
 
 class Sprites:
 
@@ -12,35 +8,13 @@ class Sprites:
         pass
 
     @staticmethod
-    def get_file(pokemon_name):
+    def get_icon(pokemon_id):
+
         found_file = None
-        for files in os.listdir(SPRITES_FOLDER):
-            if Sprites.clean(pokemon_name).lower() in files.lower():
-                found_file = os.path.join(SPRITES_FOLDER, files)
+        formatted_id = f"{pokemon_id:04d}"
+
+        for files in os.listdir(SPRITES_DIR):
+            if formatted_id in files:
+                found_file = os.path.join(SPRITES_DIR, files)
                 break
         return found_file
-    
-    @staticmethod
-    def clean(pokemon_name):
-        cleaned_name = re.sub(r'[^\w\s-]', '', pokemon_name)
-        return cleaned_name
-
-    @staticmethod
-    def download_files():
-        with request.urlopen("https://tyradex.vercel.app/api/v1/pokemon") as response:
-            status_code = response.getcode()
-            if status_code == 200:
-                data = response.read().decode('utf-8') 
-                pokedex_data = json.loads(data)
-
-        for index, pokemon in enumerate(pokedex_data):
-            sprite = Sprites.get_pokemon_sprites(pokemon)
-            with request.urlopen(sprite) as response:
-                status_code = response.getcode()
-                if status_code == 200:
-                    pokemon_cleaned_name_en = Sprites.clean_name(pokemon['name']['en'])
-                    pokemon_cleaned_name_fr = Sprites.clean_name(pokemon['name']['fr'])
-                    filename = f"{index}_{pokemon_cleaned_name_en}_{pokemon_cleaned_name_fr}.png"
-                    output_path = os.path.join(SPRITES_FOLDER, filename)
-                    with open(output_path, "wb") as f:
-                        f.write(response.read())
