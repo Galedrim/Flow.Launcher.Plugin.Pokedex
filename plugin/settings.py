cd0bd0070f695ca26ff383c46ee7bdf79c0d42ca
Galedrim <84284891+Galedrim@users.noreply.github.com>
2024-07-20 03:38:11 +0200

    :bug: Add Default Language if Settings Problems

--- plugin/settings.py
+++ plugin/settings.py
@@ -3,21 +3,22 @@ import json
 
 class Settings:
 
-    plugins_dir = os.path.dirname(os.getcwd())
-    plugins_parent_dir = os.path.dirname(plugins_dir)
-    settings_file_path = os.path.join(plugins_parent_dir, "Settings", "Settings.json")
-
-    default_language = "en"
+    DEFAULT_LANGUAGE  = "en"
 
     def __init__(self):
         pass
 
-    @staticmethod
     def get_language():
+        
+        plugins_parent_dir = os.path.dirname(os.path.dirname(os.getcwd()))
+        settings_file_path = os.path.join(plugins_parent_dir, "Settings", "Settings.json")
 
-        if os.path.exists(Settings.settings_file_path):
-            with open(Settings.settings_file_path) as f:
-                data = json.load(f)
-                return data.get('Language', Settings.default_language)
+        if os.path.exists(settings_file_path):
+            try:
+                with open(settings_file_path, 'r') as f:
+                    data = json.load(f)
+                    return data.get('Language', Settings.DEFAULT_LANGUAGE)
+            except (json.JSONDecodeError, IOError):
+                return Settings.DEFAULT_LANGUAGE
         else:
-            return Settings.default_language
+            return Settings.DEFAULT_LANGUAGE
