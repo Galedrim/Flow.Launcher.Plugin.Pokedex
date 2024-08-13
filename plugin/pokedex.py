fde232e733a678176b3b2240f4933a2f606a627e
Galedrim <84284891+Galedrim@users.noreply.github.com>
2024-08-13 18:51:29 +0200

    :sparkles: Add complete informations in english

--- plugin/pokedex.py
+++ plugin/pokedex.py
@@ -6,13 +6,13 @@ from settings import Settings
 from plugin.abilities import AbilityLoader
 from plugin.natures import NatureLoader
 from plugin.pokemons import PokemonLoader
-from plugin.types import TypeLoader
 
-COUP_CRITIQUE_ICON = r".\images\coup_critique.png"
-SMOGON_ICON = r".\images\smogon.png"
-POKEBIP_ICON = r".\images\pokebip.png"
 BULBAPEDIA_ICON = r".\images\bulbapedia.png"
+COUP_CRITIQUE_ICON = r".\images\coup_critique.png"
 PILULE_TALENT_ICON = r".\images\pilule_talent.png"
+POKEBIP_ICON = r".\images\pokebip.png"
+SEREBII_ICON = r".\images\serebii.png"
+SMOGON_ICON = r".\images\smogon.png"
 
 class Pokedex(Flox):
     def __init__(self, *args, **kwargs):
@@ -22,41 +22,116 @@ class Pokedex(Flox):
         self.pokemon_loader = PokemonLoader()
         self.nature_loader = NatureLoader()
         self.ability_loader = AbilityLoader()
-        self.type_loader = TypeLoader()
 
     def results(self, query):
 
-        language_used = self.language
+        self.pokemon_base_results(query)
+        self.pokemon_regional_results(query)
+        self.pokemon_mega_results(query)
+        self.pokemon_nature_results(query)
+        self.pokemon_ability_results(query)
 
-        for pokemon in self.pokemon_loader.pokemon_list:
-                if any(self.match(query, name) for name in pokemon.name.values() if name):
-                    self.add_item(
-                        title=f"{pokemon.get_name(language_used)} - {pokemon.get_types(language_used)}",
-                        subtitle=f"{pokemon.get_evolutions(language_used)} - {pokemon.get_abilities(language_used)}\n{pokemon.get_stats()}",
-                        icon=f"{pokemon.get_icon()}",
-                        context=pokemon.name,
-                        method=self.open_url,
-                        parameters=[self.get_pokemon_url(pokemon.name)]
-                    )
+        return self._results
 
-        for name_fr, nature in self.nature_loader.nature_dict.items():
-            if any(self.match(query, name) for name in nature.name.values() if name):
+    def pokemon_base_results(self, query):
+        for name, pokemon in self.pokemon_loader.pokemon_base_dict.items():
+            if self.language == 'fr':
+                all_names = list(pokemon.name.values())
+                for pre_evolution in pokemon.pre_evolutions.values():
+                    all_names.extend(pre_evolution)
+    
+                for next_evolution in pokemon.next_evolutions.values():
+                    all_names.extend(next_evolution)
+
+                if any(self.match(query, name) for name in all_names if name):
+                    self.add_pokemon_item(pokemon, pokemon.name['fr'])
+            else:
+                all_names = [pokemon.name['en'].lower()]
+                for pre_evolution in pokemon.pre_evolutions['en']:
+                    all_names.append(pre_evolution.lower())
+
+                for next_evolution in pokemon.next_evolutions['en']:
+                    all_names.append(next_evolution.lower())
+
+                if any(self.match(query, name) for name in all_names if name):
+                    self.add_pokemon_item(pokemon, pokemon.name['en'])
+
+    def pokemon_regional_results(self, query):
+        for name, pokemon in self.pokemon_loader.pokemon_regional_dict.items():
+            if self.language == 'fr': 
+                all_names = list(pokemon.name.values())
+                for pre_evolution in pokemon.pre_evolutions.values():
+                    all_names.extend(pre_evolution)
+    
+                for next_evolution in pokemon.next_evolutions.values():
+                    all_names.extend(next_evolution)
+
+                if any(self.match(query, name) for name in all_names if name):
+                    self.add_pokemon_item(pokemon, pokemon.name['fr'])
+
+            else:
+                all_names = [pokemon.name['en'].lower()]
+                for pre_evolution in pokemon.pre_evolutions['en']:
+                    all_names.append(pre_evolution.lower())
+
+                for next_evolution in pokemon.next_evolutions['en']:
+                    all_names.append(next_evolution.lower())   
+
+                if any(self.match(query, name) for name in all_names if name):
+                    self.add_pokemon_item(pokemon, pokemon.base_forms['en'])
+
+    def pokemon_mega_results(self, query):
+        for name, pokemon in self.pokemon_loader.pokemon_mega_dict.items():
+            if self.language == 'fr': 
+                all_names = list(pokemon.name.values())
+                for pre_evolution in pokemon.pre_evolutions.values():
+                    all_names.extend(pre_evolution)
+
+                if any(self.match(query, name) for name in all_names if name):
+                    self.add_pokemon_item(pokemon, pokemon.name['fr'])
+
+            else:
+                all_names = [pokemon.name['en'].lower()]
+                for pre_evolution in pokemon.pre_evolutions['en']:
+                    all_names.append(pre_evolution.lower())
+
+                if any(self.match(query, name) for name in all_names if name):
+                    self.add_pokemon_item(pokemon, pokemon.pre_evolutions['en'][0].lower())
+    
+    def pokemon_nature_results(self, query):
+        for name, nature in self.nature_loader.nature_dict.items():
+            if any(self.match(query, name) for name in nature.name.values()):
                 self.add_item(
-                    title=f"{nature.get_name(language_used)}",
+                    title=f"{nature.get_name(self.language)}",
                     subtitle=f"{nature.get_stats()}",
                 )
 
-        for name_en, ability in self.ability_loader.ability_dict_en.items():
-            if any(self.match(query, name) for name in ability.name.values() if name):
-                self.add_item(
-                    title=f"{ability.get_name(language_used)}",
-                    subtitle=f"{ability.get_description(language_used)}",
-                    icon=PILULE_TALENT_ICON,
-                    method=self.open_url,
-                    parameters=[self.get_ability_url(ability.name)]
-                )
+    def pokemon_ability_results(self, query):
+        for name, ability in self.ability_loader.ability_dict_en.items():
+            if any(self.match(query, name) for name in ability.name.values()):
+                if self.language == 'fr': 
+                    self.add_ability_item(ability, ability.name['fr'].lower())
+                else:
+                    self.add_ability_item(ability, ability.name['en'].lower())
 
-        return self._results
+    def add_pokemon_item(self, pokemon, pokemon_url):
+        self.add_item(
+            title=f"{pokemon.get_name(self.language)} - {pokemon.get_types(self.language)}",
+            subtitle=f"{pokemon.get_evolutions(self.language)} - {pokemon.get_abilities(self.language)}\n{pokemon.get_stats(self.language)}",
+            icon=f"{pokemon.get_icon()}",
+            context=pokemon.name,
+            method=self.open_url,
+            parameters=[self.get_pokemon_url(pokemon_url)]
+        )
+
+    def add_ability_item(self, ability, ability_url):
+        self.add_item(
+            title=f"{ability.get_name(self.language)}",
+            subtitle=f"{ability.get_description(self.language)}",
+            icon=PILULE_TALENT_ICON,
+            method=self.open_url,
+            parameters=[self.get_ability_url(ability_url)]
+        )
 
     def match(self, query, name):
         if query == "":
@@ -71,19 +146,28 @@ class Pokedex(Flox):
 
         if self.language == "fr":
             self.add_item(
-                title="Open Coupcritique.fr",
-                subtitle="Open Coupcritique.fr",
-                icon=COUP_CRITIQUE_ICON,
-                method=self.open_url,
-                parameters=[f"https://www.coupcritique.fr/search/{name['fr']}"]
-            )
-            self.add_item(
                 title="Open Pokebip.com",
                 subtitle="Open Pokebip",
                 icon=POKEBIP_ICON,
                 method=self.open_url,
                 parameters=[f"https://www.pokebip.com/pokedex/pokemon/{name['fr']}"]
             )
+            self.add_item(
+                title="Open Coupcritique.fr",
+                subtitle="Open Coupcritique.fr",
+                icon=COUP_CRITIQUE_ICON,
+                method=self.open_url,
+                parameters=[f"https://www.coupcritique.fr/search/{name['fr']}"]
+            )
+
+
+        self.add_item(
+            title="Open Serebii.com",
+            subtitle="Open Serebii.com",
+            icon=SEREBII_ICON,
+            method=self.open_url,
+            parameters=[f"https://www.serebii.net/pokemon/{name['en']}"]
+        )
 
         self.add_item(
             title="Open Smogon.com",
@@ -108,18 +192,18 @@ class Pokedex(Flox):
 
         if pokemon_name is not None:
             if self.language == 'fr': 
-                return f"https://www.coupcritique.fr/search/{pokemon_name['fr']}"
+                return f"https://www.pokebip.com/pokedex/pokemon/{pokemon_name}"
             else:
-                return f"https://bulbapedia.bulbagarden.net/wiki/{pokemon_name['en']}_(Pokémon)"
+                return f"https://www.serebii.net/pokemon/{pokemon_name}"
         return ''
 
     def get_ability_url(self, ability_name):
 
         if ability_name is not None:
             if self.language == 'fr': 
-                return f"https://www.coupcritique.fr/search/{ability_name['fr']}"
+                return f"https://www.coupcritique.fr/search/{ability_name}"
             else:
-                return f"https://bulbapedia.bulbagarden.net/wiki/{ability_name['en']}_(Ability)"
+                return f"https://bulbapedia.bulbagarden.net/wiki/{ability_name}_(Ability)"
         return ''
 
     def query(self, query):
--- plugin/sprites.py
+++ plugin/sprites.py
@@ -1,23 +1,31 @@
 import os
 
-SPRITES_DIR  = r".\sprites"
+SPRITES_DIR = r".\sprites"
 
-class Sprites:
-
-    def __init__(self):
-        pass
-
+class Sprite:
+    
+    index_map = {}
+    
     @staticmethod
-    def get_icon(pokemon_id, region):
-
+    def get_icon(pokemon_id):
         found_file = None
         formatted_id = f"{pokemon_id:04d}"
 
-        if region is not None:
-            formatted_id = f"{pokemon_id:04d}_01"
+        if pokemon_id in Sprite.index_map:
+            Sprite.index_map[pokemon_id] += 1
+        else:
+            Sprite.index_map[pokemon_id] = 0
+
+        index = Sprite.index_map[pokemon_id]
+
+        if index == 0:
+            formatted_id = f"{pokemon_id:04d}"
+        else:
+            formatted_id = f"{pokemon_id:04d}_{index:02d}"
 
         for files in os.listdir(SPRITES_DIR):
             if formatted_id in files:
                 found_file = os.path.join(SPRITES_DIR, files)
                 break
+                
         return found_file
